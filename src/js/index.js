64bc8d80c48ef0ca15a7961ad314c1c888839683
NikitaMelnychencko <hunter6059@gmail.com>
2021-08-24 15:47:13 +0300

    add_globall_tusk

--- src/js/index.js
+++ src/js/index.js
@@ -8,11 +8,20 @@
 // Очистка значения атрибута src элемента img.lightbox__image.Это необходимо для того,
 // чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
 
+// Следующий функционал не обязателен при сдаче задания, но будет хорошей практикой по работе с событиями.
 
+// Закрытие модального окна по клику на div.lightbox__overlay.
+// Закрытие модального окна по нажатию клавиши ESC.
+// Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
 
 import galleryItems from './app.js';
-console.log(galleryItems);
+//refs
 const galleryCollection = document.querySelector('.js-gallery')
+const galleryModalWindow = document.querySelector('.js-lightbox')
+const removeSrcImg = galleryModalWindow.querySelector('.lightbox__image')
+const overlayLink = galleryModalWindow.querySelector('.lightbox__overlay')
+const btnLink = galleryModalWindow.querySelector('.lightbox__button')
+
 const cardsImag = createImag(galleryItems);
 
 galleryCollection.insertAdjacentHTML('beforeend', cardsImag);
@@ -25,6 +34,7 @@ function createImag(galleryItems) {
       <a
         class="gallery__link"
         href="${original}"
+        
       >
         <img
           class="gallery__image"
@@ -41,7 +51,24 @@ function createImag(galleryItems) {
 galleryCollection.addEventListener('click', evt => {
   if (evt.target.nodeName !== 'IMG') {
     return
-   }
-  const linkCurrent = evt.target
+  }
+  galleryModalWindow.classList.add('is-open')
+  evt.preventDefault();//отмена перехода по link
+  const linkCurrent = evt.target.dataset.source
+  const altCurrent = evt.target.alt
+  removeSrcImg.src = `${linkCurrent}`
+  removeSrcImg.alt = `${altCurrent}`
   
+  
+})
+btnLink.addEventListener('click', evt => {
+  galleryModalWindow.classList.remove('is-open')
+  removeSrcImg.src = ''
+  removeSrcImg.alt = ''
+})
+
+overlayLink.addEventListener('click', evt => {
+  if (evt.currentTarget===evt.target) {
+   galleryModalWindow.classList.remove('is-open')
+ }
 })
